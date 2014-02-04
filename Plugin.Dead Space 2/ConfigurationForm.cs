036376247193aef87b02f58ffcbbe52608a366f9
David Rudie <d.rudie@gmail.com>
2014-02-03 22:30:42 -0500

    Dead Space 2 support
    Added plugin for Dead Space 2.

---
+++ Plugin.Dead Space 2/ConfigurationForm.cs
@@ -0,0 +1,185 @@
+﻿#region File Information
+/*
+ * Copyright (C) 2014 David Rudie
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ */
+#endregion
+
+namespace Plugin.DeadSpace2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Globalization;
+    using System.Reflection;
+    using System.Text;
+    using System.Windows.Forms;
+
+    public partial class ConfigurationForm : Form, IConfiguration
+    {
+        private string websiteUrl = string.Empty;
+        private string donateUrl = string.Empty;
+        private string detailedReportUrl = string.Empty;
+
+        private volatile float trackBarValue = 0.0f;
+
+        public ConfigurationForm()
+        {
+            this.InitializeComponent();
+
+            this.FormClosed += new FormClosedEventHandler(this.Configuration_FormClosed);
+
+            this.comboBoxGameVersion.DrawItem += new DrawItemEventHandler(this.ComboBoxGameVersion_DrawItem);
+
+            this.comboBoxGameVersion.Items.Add("Steam");
+
+            try
+            {
+                this.trackBarFieldOfView.Value = Properties.Settings.Default.TrackBarValue;
+                this.labelTrackBarValue.Text = Properties.Settings.Default.LabelTrackBarValue;
+
+                this.comboBoxGameVersion.SelectedIndex = Properties.Settings.Default.GameVersion;
+            }
+            catch
+            {
+                this.trackBarFieldOfView.Value = 500;
+                this.labelTrackBarValue.Text = "0";
+
+                this.comboBoxGameVersion.SelectedIndex = 0;
+
+                throw;
+            }
+
+            // Plugin information goes below here.
+            this.labelAuthor.Text = "Dopefish";
+            this.linkLabelWebsite.Text = "Widescreen Fixer";
+            this.websiteUrl = "https://www.widescreenfixer.org/";
+            this.linkLabelDonate.Text = "PayPal";
+            this.donateUrl = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&item_name=Donation%20for%20Widescreen%20Fixer%20%26%20Dead%20Space%202&hosted_button_id=10165367";
+            this.labelDescription.Text = "This plugin will override the game's field-of-view.";
+            this.detailedReportUrl = "http://www.wsgf.org/dr/dead-space-2/en";
+
+            Assembly pluginAssembly = Assembly.GetExecutingAssembly();
+            AssemblyName pluginName = pluginAssembly.GetName();
+            Version pluginVersion = pluginName.Version;
+            this.labelPluginVersion.Text = string.Format(CultureInfo.InvariantCulture, "{0}.{1} r{2}", pluginVersion.Major, pluginVersion.Minor, pluginVersion.Build);
+        }
+
+        public int GameVersion
+        {
+            get
+            {
+                return this.comboBoxGameVersion.SelectedIndex;
+            }
+        }
+
+        public float AmountToModifyFieldOfView
+        {
+            get
+            {
+                return this.trackBarValue;
+            }
+        }
+
+        private void Configuration_FormClosed(object sender, EventArgs e)
+        {
+            this.SaveSettings();
+        }
+
+        private void ComboBoxGameVersion_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            e.DrawFocusRectangle();
+
+            if (e.Index >= 0)
+            {
+                string item = (string)this.comboBoxGameVersion.Items[e.Index];
+                using (SolidBrush solidBrush = new SolidBrush(e.ForeColor))
+                {
+                    e.Graphics.DrawString(item, e.Font, solidBrush, new Point(e.Bounds.X, e.Bounds.Y + 1));
+                }
+            }
+        }
+
+        private void TrackBarFieldOfView_ValueChanged(object sender, EventArgs e)
+        {
+            this.labelTrackBarValue.Text = string.Format(CultureInfo.InvariantCulture, "{0}", (float)this.trackBarFieldOfView.Value / 100.0f);
+            this.trackBarValue = this.trackBarFieldOfView.Value / 100.0f;
+        }
+
+        private void SaveSettings()
+        {
+            // Game version
+            if (this.comboBoxGameVersion.SelectedIndex >= 0)
+            {
+                Properties.Settings.Default.GameVersion = this.comboBoxGameVersion.SelectedIndex;
+            }
+            else
+            {
+                Properties.Settings.Default.GameVersion = 0;
+            }
+
+            // Trackbar value
+            if (this.trackBarFieldOfView.Value >= 0)
+            {
+                Properties.Settings.Default.TrackBarValue = this.trackBarFieldOfView.Value;
+            }
+            else
+            {
+                Properties.Settings.Default.TrackBarValue = 0;
+            }
+
+            // Label value
+            string labelValue = string.Format(CultureInfo.InvariantCulture, "{0}", (float)this.trackBarFieldOfView.Value / 100.0f);
+            if (!string.IsNullOrEmpty(labelValue))
+            {
+                Properties.Settings.Default.LabelTrackBarValue = labelValue;
+            }
+            else
+            {
+                Properties.Settings.Default.LabelTrackBarValue = "0";
+            }
+
+            // Save settings
+            Properties.Settings.Default.Save();
+        }
+
+        private void ButtonSaveSettings_Click(object sender, EventArgs e)
+        {
+            this.SaveSettings();
+        }
+
+        private void ButtonCloseWindow_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LinkLabelWebsite_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(this.websiteUrl);
+        }
+
+        private void LinkLabelDonate_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(this.donateUrl);
+        }
+
+        private void LinkLabelWSGFDetailedReport_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(this.detailedReportUrl);
+        }
+    }
+}
---
+++ Plugin.Dead Space 2/Dead Space 2.cs
@@ -0,0 +1,175 @@
+﻿#region File Information
+/*
+ * Copyright (C) 2014 David Rudie
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ */
+#endregion
+
+namespace Plugin.DeadSpace2
+{
+    using System;
+    using System.Drawing;
+    using System.Globalization;
+    using System.Threading;
+    using System.Windows.Forms;
+    using Library.Plugin;
+    using Library.Process;
+
+    [FixerPluginAttribute("Dead Space 2")]
+    public sealed partial class DeadSpace2Game : BasePlugin, IFixer
+    {
+        #region Plugin-Specific Data
+
+        private IConfiguration configurationForm;
+
+        private int offsetFieldOfView;
+
+        private int offsetPoke;
+        private string pokeEnable;
+        private string pokeDisable;
+        private int offsetCodeCave;
+        private string pokeCodeCave;
+
+        #endregion
+
+        #region Constructor
+
+        public DeadSpace2Game() : base()
+        {
+            this.GameName = "Dead Space 2";
+            this.ValueTitle1 = "Field-of-View";
+            this.ValueTitle2 = "Add Amount";
+            this.ValueTitle3 = "Base Address";
+
+            this.GameIcon = Properties.Resources.Icon;
+
+            this.configurationForm = new ConfigurationForm();
+            this.ConfigurationForm = (ConfigurationForm)this.configurationForm;
+
+            // This data is the same no matter which version the user chooses
+            this.ExeName = "deadspace2";
+            this.WindowClass = "DeadSpace2WndClass";
+            this.WindowTitle = "Dead Space™ 2";
+
+            this.pokeEnable = @"call 0x{0:X}
+                                nop
+                                nop
+                                nop";
+
+            this.pokeDisable = @"movss [ecx+0x0000014C],xmm0";
+
+            this.pokeCodeCave = @"movss [0x{0:X}],xmm0
+                                  fld dword [0x{0:X}]
+                                  fld dword [0x{1:X}]
+                                  faddp
+                                  fstp dword [ecx+0x0000014C]
+                                  ret";
+        }
+
+        #endregion
+
+        #region Overridden Methods
+
+        public override void UpdateOffsets()
+        {
+            int gameVersion = Properties.Settings.Default.GameVersion;
+
+            switch (gameVersion)
+            {
+                case 0:
+                    this.offsetFieldOfView = 0x1C4E5FC;
+                    this.offsetPoke = 0x392116;
+                    break;
+
+                default:
+                    this.offsetFieldOfView = 0x1C4E5FC;
+                    this.offsetPoke = 0x392116;
+                    break;
+            }
+        }
+
+        public override void UpdateValues()
+        {
+            base.UpdateValues();
+
+            var fieldOfView = this.ProcessHandle.ReadMemory<float>(this.PointerBaseAddress + this.offsetFieldOfView);
+
+            this.Value1 = fieldOfView > 0.0f ? string.Format(CultureInfo.InvariantCulture, "{0:0.########}", fieldOfView) : "N/A";
+            this.Value2 = string.Format(CultureInfo.InvariantCulture, "{0:0.########}", this.configurationForm.AmountToModifyFieldOfView);
+            this.Value3 = this.BaseAddress > 0 ? string.Format(CultureInfo.InvariantCulture, "0x{0:X}", this.BaseAddress) : "N/A";
+        }
+
+        public override void Enable()
+        {
+            base.Enable();
+
+            IntPtr offsetCodeCave = ProcessFunctions.AllocateMemory<IntPtr>(this.ProcessHandle, 0x64);
+
+            this.offsetCodeCave = (int)offsetCodeCave;
+            int offsetFov = this.offsetCodeCave + 0x30;
+            int offsetAdd = this.offsetCodeCave + 0x40;
+
+            if (offsetCodeCave != null && offsetCodeCave != IntPtr.Zero)
+            {
+                // Fill in the arguments.
+                string pokeCodeCave = string.Format(CultureInfo.InvariantCulture, this.pokeCodeCave, offsetFov, offsetAdd);
+
+                // Assemble the code-cave.
+                byte[] pokeCodeCaveBytes = ProcessFunctions.Assemble(pokeCodeCave);
+
+                // Write the code-cave to memory.
+                this.ProcessHandle.WriteMemory((IntPtr)this.offsetCodeCave, pokeCodeCaveBytes);
+
+                // Calculate the call address.
+                int pokeCall = this.offsetCodeCave - (this.BaseAddress + this.offsetPoke);
+
+                // Fill in the arguments.
+                string pokeEnable = string.Format(CultureInfo.InvariantCulture, this.pokeEnable, pokeCall);
+
+                // Assemble the enable poke.
+                byte[] pokeEnableBytes = ProcessFunctions.Assemble(pokeEnable);
+
+                // Write the call to memory.
+                this.ProcessHandle.WriteMemory((IntPtr)this.BaseAddress + this.offsetPoke, pokeEnableBytes);
+            }
+
+            this.Thread.Start();
+        }
+
+        public override void Continual()
+        {
+            base.Continual();
+
+            while (!this.StopThread)
+            {
+                this.ProcessHandle.WriteMemory((UIntPtr)this.offsetCodeCave + 0x40, this.configurationForm.AmountToModifyFieldOfView);
+
+                Thread.Sleep(125);
+            }
+        }
+
+        public override void Disable()
+        {
+            base.Disable();
+
+            byte[] disableBytes = ProcessFunctions.Assemble(this.pokeDisable);
+
+            this.ProcessHandle.WriteMemory(this.PointerBaseAddress + this.offsetPoke, disableBytes);
+        }
+
+        #endregion
+    }
+}
